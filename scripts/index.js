7a08d9cf680cd909d6df0be79a0a2f1959fdb96f
Regina Nikogosian <reginanikogosian@yandex.ru>
2024-02-15 23:11:21 -0500

    Sprint5 Initial commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,44 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
-// @todo: DOM узлы
+// DOM узлы
+const cardsContainer = document.querySelector(".places__list");
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createCard(name, link, deleteFunction) {
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
 
-// @todo: Функция удаления карточки
+  cardTitle.textContent = name;
+  cardImage.src = link;
+  cardImage.alt = "Фотография места: " + name;
+  deleteButton.addEventListener("click", deleteFunction);
 
-// @todo: Вывести карточки на страницу
+  return cardElement;
+}
+
+// Функция удаления карточки
+function deleteCard(event) {
+  const cardDeleteButton = event.target;
+  const cardItem = cardDeleteButton.closest(".places__item");
+  cardItem.remove();
+}
+
+// Вывести карточки на страницу
+function renderCards(cardsArray) {
+  if (!Array.isArray(cardsArray)) {
+    console.log("Invalid cards array provided.");
+    return;
+  }
+
+  cardsArray.forEach((card) => {
+    const newCard = createCard(card.name, card.link, deleteCard);
+    cardsContainer.append(newCard);
+  });
+}
+
+renderCards(initialCards);
